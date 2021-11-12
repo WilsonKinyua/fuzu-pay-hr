d2a7ebc3ac048d7df590124922f6adf971366dee
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-11-12 23:00:55 +0300

    folder structure set up and bootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { StaffComponent } from './modules/staff/staff.component';
+import { StaffDetailsComponent } from './modules/staff/staff-details/staff-details.component';
+import { DepartmentsComponent } from './modules/staff/departments/departments.component';
+import { AddStaffComponent } from './modules/staff/add-staff/add-staff.component';
+import { LeaveManagerComponent } from './modules/leave-manager/leave-manager.component';
+import { HiringComponent } from './modules/hiring/hiring.component';
+import { JobsComponent } from './modules/hiring/jobs/jobs.component';
+import { NewListingComponent } from './modules/hiring/jobs/new-listing/new-listing.component';
+import { ApplicationsComponent } from './modules/hiring/applications/applications.component';
+import { InterviewsComponent } from './modules/hiring/interviews/interviews.component';
+import { HiredComponent } from './modules/hiring/hired/hired.component';
+import { ReportsComponent } from './modules/reports/reports.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { DatetimeComponent } from './shared/components/datetime/datetime.component';
+import { AuthDirective } from './shared/directives/auth.directive';
+import { SafePipe } from './shared/pipes/safe.pipe';
+import { CapitalizePipe } from './shared/pipes/capitalize.pipe';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    StaffComponent,
+    StaffDetailsComponent,
+    DepartmentsComponent,
+    AddStaffComponent,
+    LeaveManagerComponent,
+    HiringComponent,
+    JobsComponent,
+    NewListingComponent,
+    ApplicationsComponent,
+    InterviewsComponent,
+    HiredComponent,
+    ReportsComponent,
+    LoaderComponent,
+    DatetimeComponent,
+    AuthDirective,
+    SafePipe,
+    CapitalizePipe,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/leave-manager/leave-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-manager',
+  templateUrl: './leave-manager.component.html',
+  styleUrls: ['./leave-manager.component.css']
+})
+export class LeaveManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
