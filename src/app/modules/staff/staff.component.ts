3a9bcbabec168dd664dc8b281e3adcedf38ed7b7
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-11-13 18:59:15 +0300

    displaying staffs list done

--- src/app/modules/staff/staff.component.ts
+++ src/app/modules/staff/staff.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.component.html',
-  styleUrls: ['./staff.component.css']
+  styleUrls: ['./staff.component.css'],
 })
 export class StaffComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // change the button current state
+  onClick(event: any) {
+    const element = event.target || event.srcElement || event.currentTarget;
+    const idAttr = element.attributes.class;
+    const value = idAttr.nodeValue;
+    const buttons = document.getElementsByClassName(value);
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove('btn-current-active');
+    }
+    event.target.classList.add('btn-current-active');
   }
-
 }
