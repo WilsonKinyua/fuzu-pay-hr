01b50d231b86cbd426fe3d35e3ea507b95a0c7d9
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-11-13 21:12:33 +0300

    displaying of of active and on leave staffs done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { HiringComponent } from './modules/hiring/hiring.component';
+import { ActiveStaffComponent } from './modules/leave-manager/active-staff/active-staff.component';
 import { LeaveManagerComponent } from './modules/leave-manager/leave-manager.component';
+import { OnLeaveComponent } from './modules/leave-manager/on-leave/on-leave.component';
 import { ReportsComponent } from './modules/reports/reports.component';
 import { AddStaffComponent } from './modules/staff/add-staff/add-staff.component';
 import { DepartmentsComponent } from './modules/staff/departments/departments.component';
@@ -17,6 +19,8 @@ const routes: Routes = [
   { path: 'reports', component: ReportsComponent },
   { path: 'departments', component: DepartmentsComponent },
   { path: 'add-staff', component: AddStaffComponent },
+  { path: 'on-leave', component: OnLeaveComponent },
+  {path: 'active-staff', component: ActiveStaffComponent},
   { path: '**', redirectTo: 'dashboard' },
 ];
 
---
+++ src/app/modules/leave-manager/active-staff/active-staff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-staff',
+  templateUrl: './active-staff.component.html',
+  styleUrls: ['./active-staff.component.css']
+})
+export class ActiveStaffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
