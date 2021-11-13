7c8de52ba29a2c112f7fa9fa55096aa99435d2fb
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-11-13 14:23:09 +0300

    navigationbar and sidebar fixed, staff overview displayed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { HiringComponent } from './modules/hiring/hiring.component';
+import { LeaveManagerComponent } from './modules/leave-manager/leave-manager.component';
+import { ReportsComponent } from './modules/reports/reports.component';
+import { StaffComponent } from './modules/staff/staff.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'staff', component: StaffComponent },
+  { path: 'leave-manager', component: LeaveManagerComponent },
+  { path: 'hiring', component: HiringComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: '**', redirectTo: 'dashboard' },
 ];
 
 @NgModule({
