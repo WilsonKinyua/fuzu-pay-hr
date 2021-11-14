b304c3418a0a9a5921f5e45d151cb10a013c00dc
bensammwaniki <bensammwaniki@gmail.com>
2021-11-14 20:44:07 +0300

    completed applications page

---
+++ src/app/modules/hiring/applications/single-applicant/single-applicant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-applicant',
+  templateUrl: './single-applicant.component.html',
+  styleUrls: ['./single-applicant.component.css']
+})
+export class SingleApplicantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
