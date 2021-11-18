ca65cd3a192ba260f209f9d1bf5762336a6497ff
WilsonKinyua <wilsonkinyuam@gmail.com>
2021-11-18 19:43:37 +0300

    adding employee to the endpoint done

--- src/app/modules/staff/add-staff/add-staff.component.ts
+++ src/app/modules/staff/add-staff/add-staff.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EmployeeService } from '../../../core/services/employee.service';
 
 @Component({
   selector: 'app-add-staff',
@@ -6,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-staff.component.css'],
 })
 export class AddStaffComponent implements OnInit {
-  constructor() {}
+  constructor(private staffService: EmployeeService) {}
 
-  ngOnInit(): void {}
+  employeeCode:string;
+
+  ngOnInit(): void {
+    this.generateEmployeeCode();
+  }
 
   showTextBox(event) {
     if (event.target.value == 'Input details') {
@@ -18,5 +24,22 @@ export class AddStaffComponent implements OnInit {
       document.getElementById('input-details').style.display = 'none';
       document.getElementById('upload-documents').style.display = 'block';
     }
+  }
+
+  // generate random staff number
+  generateEmployeeCode() {
+    const year = new Date().getFullYear();
+    const randomNumber = Math.floor(Math.random() * 1000);
+    const code = Math.random().toString(36).substring(2, 4).toUpperCase();
+    this.employeeCode = `${code}/${randomNumber}/${year}`;
+    console.log(this.employeeCode);
+  }
+
+  // add staff
+  addStaffOnSubmit(form: NgForm) {
+    console.log(form.value);
+    this.staffService.addStaff(form.value).subscribe((res) => {
+      console.log(res);
+    });
   }
 }
--- src/app/shared/models/staff.ts
+++ src/app/shared/models/staff.ts
@@ -1,2 +1,17 @@
 export class Staff {
+  constructor(
+    public employee_id: string,
+    public department: number,
+    public employment_type: number,
+    public surname: string,
+    public other_names: string,
+    public work_email: string,
+    public phone_number: string,
+    public id_number: number,
+    public position: string,
+    public gross_salary: number,
+    public employment_date: Date,
+    public net_salary?: number,
+    public date_of_birth?: string
+  ) {}
 }
