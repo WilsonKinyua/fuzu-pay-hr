06e523611e96470b57ff66fdadaa486c3397ce28
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-11-22 22:39:36 +0300

    Add applicant service

---
+++ src/app/shared/models/job-listing.ts
@@ -0,0 +1,22 @@
+export class JobListing {
+    constructor(
+        public job_title: string,
+        public job_description: string,
+        public department: string,
+        public position: string,
+        public location: string,
+        public job_type: string,
+        public experience: string,
+        public salary: number,
+        public deadline: Date,
+        public created_at: Date,
+      
+        
+
+    
+    ){
+    
+
+
+    }
+}
