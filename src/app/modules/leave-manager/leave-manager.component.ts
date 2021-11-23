6882e3e58f2a0a925d46852c30760d916fa30d3b
WilsonKinyua <wilsonkinyuam@gmail.com>
2021-11-24 00:35:54 +0300

    approving employees and reject done

--- src/app/modules/leave-manager/leave-manager.component.ts
+++ src/app/modules/leave-manager/leave-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LeaveRequestService } from 'src/app/core/services/leave-request.service';
 
 @Component({
   selector: 'app-leave-manager',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave-manager.component.css'],
 })
 export class LeaveManagerComponent implements OnInit {
-  constructor() {}
+  constructor(private leaveRequestService: LeaveRequestService) {}
 
-  ngOnInit(): void {}
+  leavesRequests;
+  isLoading = false;
+  checkedInputs = [];
+  successMessage;
+  errorMessage;
+
+  ngOnInit(): void {
+    this.getLeaveRequests();
+  }
 
   // change the button current state
   onClick(event: any) {
@@ -20,5 +29,73 @@ export class LeaveManagerComponent implements OnInit {
       buttons[i].classList.remove('btn-current-active');
     }
     event.target.classList.add('btn-current-active');
+  }
+
+  // get leave requests
+  getLeaveRequests() {
+    this.isLoading = true;
+    this.leaveRequestService.getAllLeaveRequests().subscribe(
+      (res) => {
+        console.log(res);
+        this.leavesRequests = res;
+        this.isLoading = false;
+      },
+      (error) => {
+        console.log(error);
+        this.isLoading = false;
+      }
+    );
+  }
+
+  // on checked radio button add it to the array checkedInputs
+  onChecked(id) {
+    this.checkedInputs.push(id);
+  }
+
+  // on uncheck radio button remove it from the array checkedInputs
+  // onUnchecked(id) {
+  //   const index = this.checkedInputs.indexOf(id);
+  //   this.checkedInputs.splice(index, 1);
+  //   console.log(this.checkedInputs);
+  // }
+
+  // on click approve leave request loop through the array of checked inputs and update the leave request
+  onSubmitApproveLeaveRequest() {
+    this.isLoading = true;
+    this.checkedInputs.forEach((id) => {
+      this.leaveRequestService.uproveLeaveRequest(id).subscribe(
+        (res) => {
+          console.log('Approved status', res);
+          this.getLeaveRequests();
+            this.successMessage = 'Leave request approved successfully';
+            this.isLoading = false;
+        },
+        (error) => {
+          console.log(error);
+          this.errorMessage = 'Something went wrong';
+          this.isLoading = false;
+        }
+      );
+    });
+  }
+
+  // on click reject leave request loop through the array of checked inputs and update the leave request
+  onSubmitRejectLeaveRequest() {
+    this.isLoading = true;
+    this.checkedInputs.forEach((id) => {
+      this.leaveRequestService.rejectLeaveRequest(id).subscribe(
+        (res) => {
+          console.log('Rejected status', res);
+          this.getLeaveRequests();
+          this.successMessage = 'Leave request rejected successfully';
+          this.isLoading = false;
+        },
+        (error) => {
+          console.log(error);
+          this.errorMessage = 'Something went wrong';
+          this.isLoading = false;
+        }
+      );
+    });
   }
 }
